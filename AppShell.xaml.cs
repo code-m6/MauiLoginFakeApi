d8d85eec6d488cf3c03cb4e151b9ef7fb19131ff
code-m6 <saeed.almaz94@gmail.com>
2026-01-28 21:49:45 -0800

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MauiLoginFakeApi
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using MauiLoginFakeApi.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace MauiLoginFakeApi
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            // Register services and pages
+            builder.Services.AddSingleton<IAuthService, ApiAuthService>();
+            builder.Services.AddTransient<LoginPage>();
+            builder.Services.AddTransient<DashboardPage>();
+
+            return builder.Build();
+        }
+
+        private class LoginPage
+        {
+        }
+
+        private class DashboardPage
+        {
+        }
+    }
+}
